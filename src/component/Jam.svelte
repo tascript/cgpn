20a6d3f71a4368348b91225c7064b489fe6de758
tascript <wataruscript8611@gmail.com>
2021-08-31 23:43:56 +0900

    use motion

---
+++ src/component/Jam.svelte
@@ -0,0 +1,15 @@
+<script lang='ts'>
+  import { spring } from 'svelte/motion'
+
+  interface Position {
+    x: number,
+    y: number
+  }
+
+  let position = spring<Position>({ x: 0, y: 0 }, {
+    stiffness: 0.1,
+    dumping: 0.1,
+    precision: 0.1
+  })
+
+</script>
