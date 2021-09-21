3a208730ff8594ad7280f3c3b9046da336aed5b9
tascript <wataruscript8611@gmail.com>
2021-09-22 00:30:01 +0900

    use slot

--- src/component/modal/Component.svelte
+++ src/component/modal/Component.svelte
@@ -1,17 +1,15 @@
 <script lang='ts'>
   import { onMount, onDestroy } from 'svelte'
-
+  export let display: boolean
   onMount(() => {
   })
   onDestroy(() => {
   })
 </script>
 
-<div class="modal">
-  <div class="box">
-    <div>
-      <p>text</p>
-    </div>
+<div class="modal {$display ? 'modal--fadein' : 'modal--fadeout'}">
+  <div class="content">
+    <slot></slot>
   </div>
 </div>
 
@@ -22,11 +20,33 @@
     height: 100%;
     z-index: 100px;
   }
-  .box {
+  .content {
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 100%;
+  }
+  .modal--fadein {
+    animation: fadein 0.3s
+  }
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  .modal--fadeout {
+    animation: fadeout 0.3s
+  }
+  @keyframes fadeout {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
   }
 </style>
--- src/component/modal/lib/event.ts
+++
@@ -1,6 +0,0 @@
-import { isDisplay } from './store'
-
-
-export const handleMousedown = (v: boolean) => {
-  isDisplay.set(v)
-}
