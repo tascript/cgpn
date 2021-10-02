5144075c50aa61e404edc637b06765af82c8efb9
tascript <wataruscript8611@gmail.com>
2021-10-02 19:51:00 +0900

    fix card

--- src/component/Main.svelte
+++ src/component/Main.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Hamon from './hamon/Component.svelte'
   import Modal from './modal/Component.svelte'
+  import Card from './card/Component.svelte'
   let display = false
 </script>
 
@@ -10,6 +11,10 @@
     <h2>This is cgpn</h2>
   </Modal>
   <button on:click={() => (display = true)}>Modal</button>
+  <Card>
+    <span slot="title">cgpn</span>
+    <span slot="description">cgpn is omoshiroi svelte components.</span>
+  </Card>
 </main>
 
 <style>
--- src/component/card/Component.svelte
+++ src/component/card/Component.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
 </script>
 
+<div class="card--layout">
+  <h2 class="card--title">
+    <slot name="title">
+      <span>CardTitle</span>
+    </slot>
+  </h2>
+  <div class="card--description">
+    <slot name="description">
+      <p>This is card description.</p>
+    </slot>
+  </div>
+</div>
+
 <style>
+  .card--layout {
+    width: 300px;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    border-radius: 0.25rem;
+    box-shadow: 1.5px 1.5px 1.5px 1px rgba(0, 0, 0, 0.5);
+    padding: 1rem;
+  }
+  .card--title {
+    margin-top: 0rem;
+    padding-left: 0.125rem;
+  }
+  .card--description {
+    padding-left: 0.125rem;
+    font-size: 0.8rem;
+  }
 </style>
